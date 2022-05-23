04b9f7dbc36bd4981bf7dee26b4a96c9be4f8279
Shamim <yeasinshamim7@gmail.com>
2022-05-23 20:08:05 +0600

    require auth added

--- src/components/Shared/RequireAuth.js
+++ src/components/Shared/RequireAuth.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation } from 'react-router-dom';
+import auth from '../../firebase.init';
 
-const RequireAuth = () => {
-    return (
-        <div>
+const RequireAuth = ({ children }) => {
+    const [user, loading, error] = useAuthState(auth);
+    const location = useLocation();
 
-        </div>
-    );
+    if (loading) return <p>Loading...</p>
+
+    if (!user) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    return children;
 };
 
 export default RequireAuth;
